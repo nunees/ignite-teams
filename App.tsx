91bacd128d4452dee4bbbda3fd143b0f2a73b4dd
nunees <felipe_silva_nunes@hotmail.com>
2023-04-12 20:22:26 -0300

    styles: add theme provider to project

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/theme";
 import { Groups } from "@screens/Groups";
 
 export default function App() {
-  return <Groups />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
+  );
 }
