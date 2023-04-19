d10adab3b6ac6bbbb5a1c996f51d97b3f474b8c7
nunees <felipe_silva_nunes@hotmail.com>
2023-04-19 18:52:44 -0300

    feat: add Header

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,11 @@
+import { Header } from "@components/Header";
 import { StyleSheet, Text, View } from "react-native";
 import * as S from "./styles";
 
 export function Groups() {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <Header></Header>
+    </S.Container>
+  );
 }
