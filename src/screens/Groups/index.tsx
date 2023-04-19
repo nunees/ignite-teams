b58c97bb80d8b07dc6c765a0059e39bad07fab9b
nunees <felipe_silva_nunes@hotmail.com>
2023-04-19 20:02:36 -0300

    feat: create text highlight component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@components/Header";
+import { Highlight } from "@components/highlight";
 import * as S from "./styles";
 
 export function Groups() {
   return (
     <S.Container>
       <Header showBackButton />
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma"></Highlight>
     </S.Container>
   );
 }
