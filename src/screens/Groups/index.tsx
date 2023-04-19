23f606b0a3fda5835f1ee5def11a65d4a9d068d1
nunees <felipe_silva_nunes@hotmail.com>
2023-04-19 18:34:39 -0300

    feat: add fonts and ActivityIndicator component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,9 +2,5 @@ import { StyleSheet, Text, View } from "react-native";
 import * as S from "./styles";
 
 export function Groups() {
-  return (
-    <S.Container>
-      <S.Title>Groups</S.Title>
-    </S.Container>
-  );
+  return <S.Container></S.Container>;
 }
