a2d536c426d03985f7a090f34e19fcb81d957398
nunees <felipe_silva_nunes@hotmail.com>
2023-04-23 14:06:17 -0300

    feat: add Filter component

---
+++ src/components/Filter/styles.ts
@@ -0,0 +1,35 @@
+import { TouchableOpacity } from "react-native";
+import styled, { css } from "styled-components/native";
+
+export type FilterStyleProps = {
+  isActive?: boolean;
+};
+
+export const Container = styled(TouchableOpacity)<FilterStyleProps>`
+  ${({ theme, isActive }) =>
+    isActive &&
+    css`
+      border: 1px solid ${theme.COLORS.GREEN_700};
+    `}
+
+  border-radius: 4px;
+  margin-top: 10px;
+  margin-right: 12px;
+  width: 70px;
+
+  //Fix for content wrap
+  padding: 5px;
+
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  text-transform: uppercase;
+  ${({ theme }) =>
+    css`
+      font-family: ${theme.FONT_FAMILY.REGULAR};
+      font-size: ${theme.FONT_SIZE.SM}px;
+      color: ${theme.COLORS.WHITE};
+    `}
+`;
