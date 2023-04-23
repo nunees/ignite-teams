b7da172962ad0faf0535520f169eb8db60500ff0
nunees <felipe_silva_nunes@hotmail.com>
2023-04-23 16:05:42 -0300

    feat: implement filter and add it to the players screen

--- src/components/Filter/styles.ts
+++ src/components/Filter/styles.ts
@@ -10,15 +10,16 @@ export const Container = styled(TouchableOpacity)<FilterStyleProps>`
     isActive &&
     css`
       border: 1px solid ${theme.COLORS.GREEN_700};
-    `}
+    `};
 
   border-radius: 4px;
   margin-top: 10px;
+
   margin-right: 12px;
   width: 70px;
 
   //Fix for content wrap
-  padding: 5px;
+  //padding: 5px;
 
   align-items: center;
   justify-content: center;
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,15 @@
-import { Container, Form } from "./styles";
+import { useState } from "react";
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
+import { FlatList } from "react-native";
 
 export function Players() {
+  const [team, setTeam] = useState("Time A");
+  const [player, setPlayers] = useState(["Rodrigo", "Diego", "Vini"]);
   return (
     <Container>
       <Header showBackButton />
@@ -17,7 +21,23 @@ export function Players() {
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
-      <Filter title="Time A" isActive />
+
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumbersOfPlayers>{player.length}</NumbersOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
