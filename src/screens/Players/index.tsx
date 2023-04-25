657eb925e714710297c35f94da697fa700b07e7a
nunees <felipe_silva_nunes@hotmail.com>
2023-04-25 18:40:43 -0300

    feat: apply styles to Players flatlist

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -7,14 +7,16 @@ import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
 import { FlatList } from "react-native";
 import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
   const [players, setPlayers] = useState([
-    "Rodrigo",
-    "Diego",
-    "Vinicius",
-    "Felipe",
+    // "Rodrigo",
+    // "Diego",
+    // "Vinicius",
+    // "Felipe",
   ]);
   return (
     <Container>
@@ -50,7 +52,16 @@ export function Players() {
         renderItem={({ item }) => (
           <PlayerCard name={item} onRemove={() => {}} />
         )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time." />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
       />
+      <Button title="Remover Turma" type="SECONDARY" />
     </Container>
   );
 }
