3ffc118b338ad058a15f75b17f7abd4b9bf30963
nunees <felipe_silva_nunes@hotmail.com>
2023-04-26 22:20:44 -0300

    fix: screen change glitch

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -7,7 +7,7 @@ const { Navigator, Screen } = createStackNavigator();
 
 export function AppRoutes() {
   return (
-    <Navigator screenOptions={{ headerShown: false }}>
+    <Navigator screenOptions={{ headerShown: false, animationEnabled: false }}>
       <Screen name="groups" component={Groups} />
       <Screen name="players" component={Players} />
       <Screen name="new" component={NewGroup} />
