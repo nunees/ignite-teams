1e51cc174aec521dc2abd4f1e09ac3ac2dca347a
nunees <felipe_silva_nunes@hotmail.com>
2023-04-30 15:03:27 -0300

    feat: add async storage to store and fetch groups

---
+++ src/storage/group/groupCreate.ts
@@ -0,0 +1,15 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { GROUP_COLLECTION } from "@storage/storageConfig";
+import { groupGetAll } from "./groupGetAll";
+
+export async function groupCreate(newGroup: string) {
+  try {
+    const storageGroups = await groupGetAll();
+
+    const storage = JSON.stringify([...storageGroups, newGroup]);
+
+    await AsyncStorage.setItem(GROUP_COLLECTION, storage);
+  } catch (error) {
+    throw error;
+  }
+}
